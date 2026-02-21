46515f2024046e5649e12cc6f20b0eeda1ae04be
ksochacki123 <kacpersochacki@moniti.app>
2026-02-21 13:51:38 +0100

    deploy stage 2026-02-21 13:51:38

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -45,6 +45,6 @@ _flutter.loader.load({
     forceSingleThreadedSkwasm: true,
   },
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3426356965",
+    serviceWorkerVersion: "2010831811",
   },
 });
